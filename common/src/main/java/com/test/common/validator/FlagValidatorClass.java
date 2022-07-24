219b2192508422be2325e58d16dd11ecb8c2bb73
894259991@qq.com <894259991@qq.com>
2022-07-24 22:06:45 +0800

    提交代码

---
+++ common/src/main/java/com/test/common/validator/FlagValidatorClass.java
@@ -0,0 +1,27 @@
+package com.test.common.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 状态标记校验器
+ */
+public class FlagValidatorClass implements ConstraintValidator<FlagValidator,String> {
+    private String[] values;
+    @Override
+    public void initialize(FlagValidator flagValidator) {
+        this.values = flagValidator.value();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        boolean isValid = false;
+        for(int i=0;i<values.length;i++){
+            if(values[i].equals(String.valueOf(value))){
+                isValid = true;
+                break;
+            }
+        }
+        return isValid;
+    }
+}
